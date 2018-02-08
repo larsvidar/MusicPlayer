b2410b33733435defef05be488ff993d3483fcb9
Lars Vidar <larsvidar@larsvidar.no>
2018-02-08 15:28:55 +0100

    - Added palylist to the GUI.

--- js/musicplayer.jsx
+++ js/musicplayer.jsx
@@ -35,10 +35,59 @@ function Controls() {
     <div className="buttons">
       <i class="button fa fa-step-backward"></i>
       <i class="button fa fa-play"></i>
-      <i class="button fa fa-pause"></i>
       <i class="button fa fa-stop"></i>
       <i class="button fa fa-step-forward"></i>
     </div>
+  );
+}
+
+/***** PLAYLIST component *****/
+function Playlist() {
+  return (
+      <table className="playlist-table">
+        <tr>
+          <th className="title">Title</th>
+          <th className="artist">Artist</th>
+          <th className="duration">Duration</th>
+          <th className="edit"></th>
+        </tr>
+        <tr>
+          <td>Sexy Boy</td>
+          <td>Air</td>
+          <td>3:21</td>
+          <td><i class="fa fa-edit"></i></td>
+        </tr>
+        <tr>
+          <td>Last Junkie</td>
+          <td>Dandy Warholes</td>
+          <td>4:03</td>
+          <td><i class="fa fa-edit"></i></td>
+        </tr>
+        <tr>
+          <td>Eple</td>
+          <td>Röyksopp</td>
+          <td>2:58</td>
+          <td><i class="fa fa-edit"></i></td>
+        </tr>
+        <tr>
+          <td>Sexy Boy</td>
+          <td>Air</td>
+          <td>3:21</td>
+          <td><i class="fa fa-edit"></i></td>
+        </tr>
+        <tr>
+          <td>Last Junkie</td>
+          <td>Dandy Warholes</td>
+          <td>4:03</td>
+          <td><i class="fa fa-edit"></i></td>
+        </tr>
+        <tr>
+          <td>Eple</td>
+          <td>Röyksopp</td>
+          <td>2:58</td>
+          <td><i class="fa fa-edit"></i></td>
+        </tr>
+      </table>
   );
 }
 
@@ -59,6 +108,9 @@ class Application extends React.Component {
           <Displaysong />
           <Progress />
           <Controls />
+          <div className="playlist">
+            <Playlist />
+          </div>
         </div>
       </div>
     );
