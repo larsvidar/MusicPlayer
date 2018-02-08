852e3e7693a124077b78bfa8eacca707df8f133c
Lars Vidar <larsvidar@larsvidar.no>
2018-02-08 20:34:08 +0100

    - Made a playlist object and got the playlist component to get it songs from it.

--- js/musicplayer.jsx
+++ js/musicplayer.jsx
@@ -1,3 +1,17 @@
+/***** Songs array *****/
+let songs = [
+  {id: 1,   songTitle: "Sexy Boy",                    artist: "Air",                duration: 214,  url: "#"},
+  {id: 2,   songTitle: "Last Junkie",                 artist: "The Dandy Warholes", duration: 324,  url: "#"},
+  {id: 3,   songTitle: "Sommerlykke",                 artist: "SNoB",               duration: 186,  url: "#"},
+  {id: 4,   songTitle: "Paris Is Burning",            artist: "Ladyhawke",          duration: 204,  url: "#"},
+  {id: 5,   songTitle: "Yesterday",                   artist: "Beatles",            duration: 151,  url: "#"},
+  {id: 6,   songTitle: "Eple",                        artist: "Röyksopp",           duration: 175,  url: "#"},
+  {id: 7,   songTitle: "Happy Up Here",               artist: "Röyksopp",           duration: 121,  url: "#"},
+  {id: 8,   songTitle: "Where the wild roses grow",   artist: "Nick Cave",          duration: 254,  url: "#"},
+  {id: 9,   songTitle: "Bently Rythm Gonna Get You",  artist: "Bently Rythm Ace",   duration: 300,  url: "#"},
+  {id: 10,  songTitle: "Stan",                        artist: "Eminem",             duration: 129,  url: "#"},
+];
+
 /***** HEADER component *****/
 function Header(props) {
   return (
@@ -42,53 +56,21 @@ function Controls() {
 }
 
 /***** PLAYLIST component *****/
-function Playlist() {
+function Playlist(props) {
   return (
-      <table className="playlist-table">
         <tr>
-          <th className="title">Title</th>
-          <th className="artist">Artist</th>
-          <th className="duration">Duration</th>
-          <th className="edit"></th>
-        </tr>
-        <tr>
-          <td>Sexy Boy</td>
-          <td>Air</td>
-          <td>3:21</td>
+          <td>{props.songtitle}</td>
+          <td>{props.artist}</td>
+          <td>{props.duration}</td>
           <td><i class="fa fa-edit"></i></td>
         </tr>
-        <tr>
-          <td>Last Junkie</td>
-          <td>Dandy Warholes</td>
-          <td>4:03</td>
-          <td><i class="fa fa-edit"></i></td>
-        </tr>
-        <tr>
-          <td>Eple</td>
-          <td>Röyksopp</td>
-          <td>2:58</td>
-          <td><i class="fa fa-edit"></i></td>
-        </tr>
-        <tr>
-          <td>Sexy Boy</td>
-          <td>Air</td>
-          <td>3:21</td>
-          <td><i class="fa fa-edit"></i></td>
-        </tr>
-        <tr>
-          <td>Last Junkie</td>
-          <td>Dandy Warholes</td>
-          <td>4:03</td>
-          <td><i class="fa fa-edit"></i></td>
-        </tr>
-        <tr>
-          <td>Eple</td>
-          <td>Röyksopp</td>
-          <td>2:58</td>
-          <td><i class="fa fa-edit"></i></td>
-        </tr>
-      </table>
   );
+}
+
+Playlist.propTypes = {
+  songtitle: PropTypes.string.isRequired,
+  artist: PropTypes.string.isRequired,
+  duration: PropTypes.number.isRequired,
 }
 
 /***** APPLICATION class *****/
@@ -96,7 +78,8 @@ class Application extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: "Music Player"
+      title: "Music Player",
+      songlist: this.props.initialPlaylist,
     };
   }
 
@@ -109,7 +92,22 @@ class Application extends React.Component {
           <Progress />
           <Controls />
           <div className="playlist">
-            <Playlist />
+            <table className="playlist-table">
+              <tr>
+                <th className="title">Title</th>
+                <th className="artist">Artist</th>
+                <th className="duration">Duration</th>
+                <th className="edit"></th>
+              </tr>
+              {this.state.songlist.map(function(song) {
+                return (
+                  <Playlist
+                    songtitle={song.songTitle}
+                    artist={song.artist}
+                    duration={song.duration} />
+                );
+              }.bind(this))}
+            </table>
           </div>
         </div>
       </div>
@@ -118,8 +116,15 @@ class Application extends React.Component {
 }
 
 Application.propTypes = {
-  title: PropTypes.string.isRequired
+  title:            PropTypes.string.isRequired,
+  initialPlaylist:  PropTypes.arrayOf(PropTypes.shape({
+    id:               PropTypes.number.isRequired,
+    songTitle:        PropTypes.string.isRequired,
+    artist:           PropTypes.string.isRequired,
+    duration:         PropTypes.number.isRequired,
+    url:              PropTypes.string.isRequired,
+  })).isRequired
 };
 
 /***** Launching the app *****/
-ReactDOM.render(<Application />, document.getElementById("container"));
+ReactDOM.render(<Application initialPlaylist={songs} />, document.getElementById("container"));
