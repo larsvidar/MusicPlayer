d3cd32fd751c43631162208545a1b9a01563ffe3
Lars Vidar <larsvidar@larsvidar.no>
2018-02-21 20:49:16 +0100

    - The playlist now retrives the correct duration from the song itself.

--- js/musicplayer.jsx
+++ js/musicplayer.jsx
@@ -1,21 +1,35 @@
 /***** Songs array *****/
 //Index 0 is reserved for stopped state.
 let songs = [
-  {id: 0,   songTitle: "",                              artist: "",                   duration: 0,    url: "#"},
-  {id: 1,   songTitle: "A Kind Of Magic",               artist: "Queen",              duration: 214,  url: "http://hcmaslov.d-real.sci-nnov.ru/public/mp3/Queen/Queen%20'A%20Kind%20Of%20Magic'.mp3"},
-  {id: 2,   songTitle: "Dynamite",                      artist: "Scorpions",          duration: 324,  url: "http://hcmaslov.d-real.sci-nnov.ru/public/mp3/Scorpions/Scorpions%20'Dynamite'.mp3"},
-  {id: 3,   songTitle: "Got My Mind Set On You",        artist: "George Harrison",    duration: 186,  url: "http://dora-robo.com/muzyka/70's-80's-90's%20/George%20Harrison%20-%20Got%20My%20Mind%20Set%20On%20You%20%5bJanuary%201988%5d.mp3"},
-  {id: 4,   songTitle: "Bohemian Rhapsody",             artist: "Queen",              duration: 204,  url: "http://hcmaslov.d-real.sci-nnov.ru/public/mp3/Queen/Queen%20'Bohemian%20Rhapsody'.mp3"},
-  {id: 5,   songTitle: "Mastermind",                    artist: "Mike Oldfield",      duration: 151,  url: "http://www.replicaradio.ro/audio/oldfield/millenium/07.Mike%20Oldfield-Mastermind.mp3"},
-  {id: 6,   songTitle: "Heart Of Glass",                artist: "Blondie",            duration: 175,  url: "http://dora-robo.com/muzyka/70's-80's-90's%20/Blondie%20-%20Heart%20of%20Glass.mp3"},
-  {id: 7,   songTitle: "End Game",                      artist: "Taylor Swift",       duration: 121,  url: "http://s1.mmdl.xyz/1396/08/18/Taylor%20Swift%20-%20Reputation/2%20End%20Game.mp3"},
-  {id: 8,   songTitle: "Ma Baker",                      artist: "Boney M",            duration: 254,  url: "http://dora-robo.com/muzyka/70's-80's-90's%20/Boney%20M.%20-%20Ma%20Baker.mp3"},
-  {id: 9,   songTitle: "Cocaine",                       artist: "Eric Clapton",       duration: 300,  url: "http://195.122.253.112/public/mp3/Eric%20Clapton/Eric%20Clapton%20'Cocaine'.mp3"},
-  {id: 10,  songTitle: "All That She Wants",            artist: "Ace Of Base",        duration: 129,  url: "http://dora-robo.com/muzyka/70's-80's-90's%20/All%20That%20She%20Wants.mp3"},
+  {id: 0,   songTitle: "",                        artist: "",                 url: "#"},
+  {id: 1,   songTitle: "A Kind Of Magic",         artist: "Queen",            url: "http://hcmaslov.d-real.sci-nnov.ru/public/mp3/Queen/Queen%20'A%20Kind%20Of%20Magic'.mp3"},
+  {id: 2,   songTitle: "Dynamite",                artist: "Scorpions",        url: "http://hcmaslov.d-real.sci-nnov.ru/public/mp3/Scorpions/Scorpions%20'Dynamite'.mp3"},
+  {id: 3,   songTitle: "Got My Mind Set On You",  artist: "George Harrison",  url: "http://dora-robo.com/muzyka/70's-80's-90's%20/George%20Harrison%20-%20Got%20My%20Mind%20Set%20On%20You%20%5bJanuary%201988%5d.mp3"},
+  {id: 4,   songTitle: "Bohemian Rhapsody",       artist: "Queen",            url: "http://hcmaslov.d-real.sci-nnov.ru/public/mp3/Queen/Queen%20'Bohemian%20Rhapsody'.mp3"},
+  {id: 5,   songTitle: "Mastermind",              artist: "Mike Oldfield",    url: "http://www.replicaradio.ro/audio/oldfield/millenium/07.Mike%20Oldfield-Mastermind.mp3"},
+  {id: 6,   songTitle: "Heart Of Glass",          artist: "Blondie",          url: "http://dora-robo.com/muzyka/70's-80's-90's%20/Blondie%20-%20Heart%20of%20Glass.mp3"},
+  {id: 7,   songTitle: "End Game",                artist: "Taylor Swift",     url: "http://s1.mmdl.xyz/1396/08/18/Taylor%20Swift%20-%20Reputation/2%20End%20Game.mp3"},
+  {id: 8,   songTitle: "Ma Baker",                artist: "Boney M",          url: "http://dora-robo.com/muzyka/70's-80's-90's%20/Boney%20M.%20-%20Ma%20Baker.mp3"},
+  {id: 9,   songTitle: "Cocaine",                 artist: "Eric Clapton",     url: "http://195.122.253.112/public/mp3/Eric%20Clapton/Eric%20Clapton%20'Cocaine'.mp3"},
+  {id: 10,  songTitle: "All That She Wants",      artist: "Ace Of Base",      url: "http://dora-robo.com/muzyka/70's-80's-90's%20/All%20That%20She%20Wants.mp3"},
 ];
 
 const audio = new Audio
 let songIndex = 11;
+setDurations();
+
+// Function for setting duration for all songs on the playlist.
+function setDurations() {
+  for (let i = 1; i < songs.length; i++) {
+    const tempAudio = new Audio();
+    tempAudio.src = songs[i].url;
+    tempAudio.load();
+    tempAudio.onloadedmetadata = () => {
+      songs[i].duration = Math.floor(tempAudio.duration);
+    }
+  }
+}
+
 
 /***** HEADER component *****/
 function Header(props) {
@@ -127,7 +141,6 @@ AddSongForm.propTypes = {
 };
 
 
-
 /***** DISPLAYSONG component *****/
 function Displaysong(props) {
   return (
@@ -151,11 +164,16 @@ Displaysong.propTypes = {
 /***** PROGRESS component *****/
 function Progress(props) {
   let lineStyle = {width: props.progressStyle + "%"}
+  let passedMinutes = Math.floor(props.timePassed / 60);
+  let passedSeconds = (props.timePassed % 60).toLocaleString(undefined, {minimumIntegerDigits: 2});
+  let lengthMinutes = Math.floor(props.timeLength / 60);
+  let lengthSeconds = (props.timeLength % 60).toLocaleString(undefined, {minimumIntegerDigits: 2});
+
   return (
     <div>
       <div className="time">
-        <p>{props.timePassed}</p>
-        <p>{props.timeLength}</p>
+        <p>{passedMinutes}:{passedSeconds}</p>
+        <p>{lengthMinutes}:{lengthSeconds}</p>
       </div>
       <div className="progress-meter">
         <div className="progress" style={lineStyle}>
@@ -208,12 +226,12 @@ function PlayListBox(props) {
           <th className="edit"></th>
         </tr>
         {props.songlist.map(function(song) {
-          console.log(song);
           return (
             <Playlist
               songtitle={song.songTitle}
               artist={song.artist}
-              duration={audio.duration} />
+              duration={song.duration}
+              key={song.id} />
           );
         }.bind(this))}
       </table>
@@ -234,11 +252,13 @@ PlayListBox.propTypes = {
 
 /***** PLAYLIST component *****/
 function Playlist(props) {
+  let minutes = Math.floor(props.duration / 60);  //Finds how many minutes the song is.
+  let seconds = (props.duration % 60).toLocaleString(undefined, {minimumIntegerDigits: 2}); //Finds the seconds, and add a zero if the number is below 10.
   return (
         <tr>
           <td>{props.songtitle}</td>
           <td>{props.artist}</td>
-          <td>{props.duration}</td>
+          <td>{minutes}:{seconds}</td>
           <td><i class="fa fa-edit"></i></td>
         </tr>
   );
@@ -287,15 +307,12 @@ class Application extends React.Component {
 
   //Updates song timers.
   updateSongTime() {
-    console.log("Update");
     this.setState({
       songLength: Math.floor(audio.duration),
       songPassed: Math.floor(audio.currentTime),
       songProgress: ( (100 / audio.duration) * audio.currentTime ),
     });
   }
-
-
 
   //Handles the skip forward and skip backward buttons.
   navigate(diff) {
@@ -352,10 +369,14 @@ class Application extends React.Component {
   }
 
   componentDidMount() {
-    //this.interval = setInterval(this.updateSongTime, 100);
+    this.interval = setInterval(this.updateSongTime, 100);
     audio.addEventListener("ended", function() {
       this.navigate(1);
     }.bind(this));
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
   }
 
   addSong(title, artist, url) {
