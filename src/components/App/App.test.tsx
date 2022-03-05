3f5472316d7673d64f702a9841083717bdb4369c
Lars Vidar Nordli <larsvidar@larsvidar.no>
2022-03-05 20:38:29 +0100

    Fixed test

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
+import ReactDOM from 'react-dom';
+
+
+it('renders without crashing', () => {
+	const div = document.createElement('div');
+	ReactDOM.render(<App />, div);
+	ReactDOM.unmountComponentAtNode(div);
+});
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+	render(<App />);
+	const titleElement = screen.getByText(/music player/i);
+	expect(titleElement).toBeInTheDocument();
 });
